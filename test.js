dad8902ba74e3efec2fdc0d4caa75ed31e464414
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-08 17:56:17 -0500

    Use object for test cases

--- test.js
+++ test.js
@@ -4,10 +4,17 @@ const pp = require(".");
 const assert = require("chai").assert;
 
 Object.entries({
-	any: ["example", ["*"]],
-	basic: ["example.hello", ["example.hello"]],
+	any: {
+		needed: "example",
+		nodes: ["*"],
+	},
+	basic: {
+		needed: "example.hello",
+		nodes: ["example.hello"],
+	},
 }).forEach(([ key, value ]) => {
 	it(key, () => {
-		assert.isTrue(typeof pp.test(...value) === "boolean");
+		const test = pp.test(value.needed, value.nodes);
+		assert.isTrue(value.invert ? !test : test);
 	});
 });
