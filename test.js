ccafb7dce069ca9b1a4b6a664315a9599bfee0fb
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-08 17:52:15 -0500

    Simplify test.js

--- test.js
+++ test.js
@@ -3,22 +3,11 @@
 const pp = require(".");
 const assert = require("chai").assert;
 
-/**
- * Test something.
- * @param {*} result The result of the test.
- */
-function test(result) {
-	it("returns boolean", () => {
-		assert.isTrue(typeof result === "boolean");
-	});
-}
-
-describe("test", () => {
-	const tests = {
-		any: pp.test("example", ["*"]),
-		basic: pp.test("example.hello", ["example.hello"]),
-	};
-	Object.keys(tests).forEach(key => {
-		describe(key, () => test(tests[key]));
+Object.entries({
+	any: ["example", ["*"]],
+	basic: ["example.hello", ["example.hello"]],
+}).forEach(([ key, value ]) => {
+	it(key, () => {
+		assert.isTrue(typeof pp.test(...value) === "boolean");
 	});
 });
