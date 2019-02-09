e654a54621a6d8a1348d6db3af6a78a02e33fa4a
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 07:53:14 -0500

    Add tests for permission selector validator

--- test.js
+++ test.js
@@ -3,6 +3,29 @@
 const pp = require(".");
 const assert = require("chai").assert;
 
+describe("validate", () => {
+	it("returns false for invalid nodes", () => {
+		[
+			"node.",
+			"node..hi",
+			"node.*e",
+			"node.**",
+		].forEach((item, index) => {
+			assert.isFalse(pp.validate(item), "invalid test " + (index + 1));
+		});
+	});
+	it("returns true for valid nodes", () => {
+		[
+			"node",
+			"node.*",
+			"node.e",
+			"node.e.f",
+		].forEach((item, index) => {
+			assert.isTrue(pp.validate(item), "valid test " + (index + 1));
+		});
+	});
+});
+
 Object.entries({
 	any: {
 		needed: "example",
