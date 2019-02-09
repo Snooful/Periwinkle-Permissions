4ed91b5f1b1f85c0f5df31aeff5cea15a938c32b
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 07:56:22 -0500

    Move permission tester tests back to their own group

--- test.js
+++ test.js
@@ -28,31 +28,33 @@ describe("validate", () => {
 	});
 });
 
-Object.entries({
-	any: {
-		needed: "example",
-		nodes: ["*"],
-	},
-	basic: {
-		needed: "example.hello",
-		nodes: ["example.hello"],
-	},
-	childNotSpecificallyMatched: {
-		invert: true,
-		needed: "example.hello",
-		nodes: ["example"],
-	},
-	childSpecificallyMatchedWithAsterisk: {
-		needed: "example.hello",
-		nodes: ["example.*"],
-	},
-}).forEach(([ key, value ]) => {
-	it(key, () => {
-		const test = pp.test(value.needed, value.nodes);
-		if (value.invert) {
-			assert.isFalse(test);
-		} else {
-			assert.isTrue(test);
-		}
+describe("test", () => {
+	Object.entries({
+		any: {
+			needed: "example",
+			nodes: ["*"],
+		},
+		basic: {
+			needed: "example.hello",
+			nodes: ["example.hello"],
+		},
+		childNotSpecificallyMatched: {
+			invert: true,
+			needed: "example.hello",
+			nodes: ["example"],
+		},
+		childSpecificallyMatchedWithAsterisk: {
+			needed: "example.hello",
+			nodes: ["example.*"],
+		},
+	}).forEach(([ key, value ]) => {
+		it(key, () => {
+			const test = pp.test(value.needed, value.nodes);
+			if (value.invert) {
+				assert.isFalse(test);
+			} else {
+				assert.isTrue(test);
+			}
+		});
 	});
 });
