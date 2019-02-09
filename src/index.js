056072196a22d8abc0fdd289aee765f89f6a3c67
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-02-09 15:04:20 -0500

    Allow sorting permission selectors

--- src/index.js
+++ src/index.js
@@ -10,12 +10,64 @@ function validate(perm) {
 		return false;
 	}
 
+	if (perm.startsWith("-")) {
+		perm = perm.slice(1);
+	}
+
 	const split = perm.split(".");
 	return split.every(part => {
 		return validPartRegex.test(part);
 	});
 }
 module.exports.validate = validate;
+
+/**
+ * Sorts permission selectors by priority.
+ * @param {string[]} permissions The permission selectors to sort.
+ * @returns {string[]} The sorted permission selectors.
+ */
+function sort(permissions = []) {
+	if (!Array.isArray(permissions)) {
+		throw new TypeError("The permissions parameter must be an array.");
+	} else if (permissions.some(perm => !validate(perm))) {
+		throw new TypeError("Permission selectors must be valid.");
+	}
+
+	return permissions.sort((a, b) => {
+		const aDash = a.startsWith("-");
+		const bDash = b.startsWith("-");
+
+		// Negated permission selectors are at the end of the array
+		if (aDash && !bDash) {
+			return 1;
+		} else if (!aDash && bDash) {
+			return -1;
+		} else if (aDash && bDash) {
+			// Thye cancel each other out
+			a = a.slice(1);
+			b = b.slice(1);
+		}
+
+		// More specific permission selectors are last
+		const specificity = a.split(".").length - b.split(".").length;
+		if (specificity) {
+			return specificity;
+		}
+
+		const aMatchesAll = a.endsWith("*");
+		const bMatchesAll = b.endsWith("*");
+
+		// Prefer specific permissions rather than matching all
+		if (aMatchesAll && !bMatchesAll) {
+			return -1;
+		} else if (!aMatchesAll && bMatchesAll) {
+			return 1;
+		} else {
+			return 0;
+		}
+	});
+}
+module.exports.sort = sort;
 
 /**
  * Tests for a permission from an array of permission selectors.
@@ -32,16 +84,25 @@ module.exports.test = (testFor, permissions = []) => {
 		throw new TypeError("Permission selectors must be valid.");
 	}
 
+	const sortedPerms = permissions.sort(perm => {
+
+	});
+
+	let exactMatchFound = false;
+	let wildcard;// = level;
+
 	return permissions.some(permission => {
 		if (permission === testFor) {
-			return true;
+			return exactMatchFound = true;
 		} else {
 			const groups = permission.split(".");
 			if (groups[groups.length - 1] === "*") {
 				const testGroups = testFor.split(".");
 				const groupsBefore = groups.slice(0, -1).join(".");
-				return testGroups.length >= groups.length && testFor.startsWith(groupsBefore);
+				return wildcard = testGroups.length >= groups.length && testFor.startsWith(groupsBefore);
 			}
 		}
 	});
+
+	return isAllowed;
 };
